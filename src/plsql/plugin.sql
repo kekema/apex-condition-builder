66179a47e4381afd732f566f5ccdd7634058acca
Karel Ekema <krl.ekema@gmail.com>
2025-06-28 17:49:21 +0200

    Update plugin.sql

--- src/plsql/plugin.sql
+++ src/plsql/plugin.sql
@@ -4,9 +4,12 @@ procedure render (
     p_param  in            apex_plugin.t_region_render_param,
     p_result in out nocopy apex_plugin.t_region_render_result )
 is 
-    l_region_id             varchar2(50); 
-    l_lang                  varchar2(5); 
+    l_region_id             varchar2(50);
+    l_lang                  varchar2(5);
     l_loc_transl_files      varchar2(200);
+    l_init_plsql_block      varchar2(4000);
+    l_qb_config             clob;
+    l_qb_config_obj         JSON_OBJECT_T;
 begin
     if apex_application.g_debug then
         apex_plugin_util.debug_region(p_plugin => p_plugin, p_region => p_region);
@@ -16,6 +19,20 @@ begin
     l_loc_transl_files := nvl(p_region.attributes.get_varchar2('attr_qb_loc_transl_files'), '#WORKSPACE_FILES#i18n/query-builder');
     if substr(l_loc_transl_files, -1) != '/' then
         l_loc_transl_files := l_loc_transl_files || '/';
+    end if;
+
+    -- get any QB config from pl/sql init anonymous block
+    l_init_plsql_block := p_region.attributes.get_varchar2('attr_init_plsql');
+    if l_init_plsql_block is not null then
+        execute immediate l_init_plsql_block using out l_qb_config;        
+    end if;
+    if l_qb_config is not null then
+        -- make sure it is valid JSON
+        l_qb_config_obj := JSON_OBJECT_T.parse(l_qb_config);
+    else
+        -- JSON.parse('null') will result in null
+        -- see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
+        l_qb_config := 'null';
     end if;
  
     sys.htp.p('<div id="' || l_region_id || '_qb"></div>');
@@ -55,10 +72,11 @@ begin
     );    
 
     -- add call to init in js
-    apex_javascript.add_inline_code(
+    apex_javascript.add_onload_code(
         p_code => apex_string.format(
-            'lib4x.axt.conditionBuilder._init("%s", '
+            'lib4x.axt.conditionBuilder._init(''%s'', '
             , l_region_id
-        ) || p_region.init_javascript_code || ');'
-    );    
+        ) || 'JSON.parse(' || '''' || apex_escape.json_clob(l_qb_config) || '''' || '), ' || p_region.init_javascript_code || ');'
+    );  
+    -- including the l_qb_config in the format function is failing as it aborts the string at 1K  
 end;
